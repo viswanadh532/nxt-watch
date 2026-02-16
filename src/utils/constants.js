70f7e8464b9c0f699b87751a3c41aa5d52b6f0da
viswanadh <viswanadh532@gmail.com>
2026-02-16 17:25:35 +0530

    updated feature / fixes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
-export const loginApiUrl = '/api/login'
-export const homeVideosApiUrl = '/api/videos/all'
-export const trendingVideosApiUrl = '/api/videos/trending'
-export const gamingVideosApiUrl = '/api/videos/gaming'
-export const videoItemDetailsApiUrl = '/api/videos'
+export const loginApiUrl = 'https://apis.ccbp.in/login'
+
+export const homeVideosApiUrl = 'https://apis.ccbp.in/videos/all?search='
+
+export const trendingVideosApiUrl = 'https://apis.ccbp.in/videos/trending'
+
+export const gamingVideosApiUrl = 'https://apis.ccbp.in/videos/gaming'
+
+export const videoItemDetailsApiUrl = 'https://apis.ccbp.in/videos'
